6ca0f51ffdf6275adb36e7551be73c01d4584790
Benjamin Lopez <contact@scotow.com>
2018-10-16 12:19:16 +0200

    refactor api

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,32 +1,72 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"github.com/tomasen/realip"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/scotow/youtubelink"
 )
 
 const (
+	invalidMethod	= "invalid http method"
 	invalidVideo 	= "invalid youtube id or link"
 	invalidBody 	= "cannot read request body"
+	invalidSourceIp	= "cannot set source ip"
 	directLinkError	= "cannot get video direct link"
 )
 
-func parseGetRequest(w http.ResponseWriter, r *http.Request) {
-	log.Println(realip.FromRequest(r))
+var (
+	useClientIp *bool
+)
 
-	request := youtubelink.Request{}
-	err := request.AddVideoLink(string(r.URL.Path[1:]))
+func parseUrl(yt *youtubelink.Request, r *http.Request) error {
+	return yt.AddVideoLink(string(r.URL.Path[1:]))
+}
+
+func parseBody(yt *youtubelink.Request, r *http.Request) error {
+	body, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		return errors.New(invalidBody)
+	}
+
+	return yt.AddVideoLink(string(body))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" && r.Method != "POST" {
+		http.Error(w, invalidMethod, http.StatusMethodNotAllowed)
+		return
+	}
+
+	yt := youtubelink.Request{}
+	err := parseUrl(&yt, r)
+
+	if err != nil && r.Method == "POST" {
+		err = parseBody(&yt, r)
+	}
 
 	if err != nil {
 		http.Error(w, invalidVideo, http.StatusNotAcceptable)
 		return
 	}
 
-	directLink, err := request.VideoLink()
+	if *useClientIp {
+		log.Println(realip.FromRequest(r))
+		err = yt.AddSourceIp(realip.FromRequest(r))
+		if err != nil {
+			http.Error(w, invalidSourceIp, http.StatusInternalServerError)
+			return
+		}
+	}
+
+	directLink, err := yt.VideoLink()
 
 	if err != nil {
 		http.Error(w, directLinkError, http.StatusInternalServerError)
@@ -36,43 +76,13 @@ func parseGetRequest(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, directLink, http.StatusFound)
 }
 
-func parsePostRequest(w http.ResponseWriter, r *http.Request) {
-	request := youtubelink.Request{}
-	err := request.AddVideoLink(r.URL.Path[1:])
-
-	if err != nil {
-		body, err := ioutil.ReadAll(r.Body)
-		defer r.Body.Close()
-
-		if err != nil {
-			http.Error(w, invalidBody, http.StatusInternalServerError)
-			return
-		}
-
-		err = request.AddVideoLink(string(body))
-		if err != nil {
-			http.Error(w, invalidVideo, http.StatusNotAcceptable)
-			return
-		}
+func listeningAddress() string {
+	port, set := os.LookupEnv("PORT")
+	if !set {
+		port = "8080"
 	}
 
-	directLink, err := request.VideoLink()
-
-	if err != nil {
-		http.Error(w, directLinkError, http.StatusInternalServerError)
-		return
-	}
-
-	http.Redirect(w, r, directLink, http.StatusFound)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		parseGetRequest(w, r)
-	case "POST":
-		parsePostRequest(w, r)
-	}
+	return ":" + port
 }
 
 func main() {
@@ -80,6 +90,9 @@ func main() {
 		log.Fatalln("youtube-dl package is not installed or cannot be found")
 	}
 
+	useClientIp = flag.Bool("i", false, "use real client ip")
+	flag.Parse()
+
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(listeningAddress(), nil))
 }
